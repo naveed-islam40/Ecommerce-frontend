6ede812da7bf1541618ba28790652bb82eaa75f8
naveed-islam40 <naveedislam20ni@gmail.com>
2024-09-16 06:06:03 -0700

    changes in json file

--- src/components/UpdateProduct/UpdateProduct.jsx
+++ src/components/UpdateProduct/UpdateProduct.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductData } from "../../action/action";
-import Carousel from "react-material-ui-carousel";
 import "./updateproduct.css";
 import { useSelector, useDispatch } from "react-redux";
 import { updateProductAction } from "../../action/updateProductDetail";
@@ -47,23 +46,41 @@ const UpdateProduct = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+
   return (
     <div className="container">
       {product && (
         <>
           <h1>Update Product</h1>
           <form onSubmit={handleSubmit}>
-            <Carousel className="imagesContainer">
-              {product.images &&
-                product.images.map((image, i) => (
-                  <img
-                    src={image.url}
-                    key={image.url}
-                    alt={`${i} image`}
-                    className="productImages"
-                  />
-                ))}
-            </Carousel>
+          <div className="w-[550px] max-md:w-96 p-5 ">
+              <Slider {...settings}>
+                {product.images &&
+                  product.images.map((src, index) => (
+                    <div key={index}>
+                      <img
+                        src={src.url}
+                        alt={`Carousel image ${index + 1}`}
+                        style={{
+                          width: "100%",
+                          height: "400px",
+                          objectFit: "cover",
+                        }}
+                        className="rounded-md mt-[100px] mr-6 max-md:w-40"
+                      />
+                    </div>
+                  ))}
+              </Slider>
+            </div>
             <div className="updateInputs">
               <input
                 type="text"
