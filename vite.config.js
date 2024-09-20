34406dca404650480f5706fc3759195dc1791633
naveed-islam40 <naveedislam20ni@gmail.com>
2024-09-20 02:49:11 -0700

    changes in fetching

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://ecommerce-backend-c405q1cfy-naveed-islams-projects-77b7560f.vercel.app",
-    },
-  },
   optimizeDeps: {
     exclude: ["country-state-picker"],
   },
