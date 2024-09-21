0792e2a96820405c2d7f0df13ae7927622eeec89
naveed-islam40 <naveedislam20ni@gmail.com>
2024-09-20 23:50:33 -0700

    changes in vite.config.json

--- src/action/action.js
+++ src/action/action.js
@@ -35,7 +35,7 @@ export const getProduct = (
     } catch (error) {
       dispatch({
         type: ALL_PRODUCTS_FAIL,
-        payload: error.response.data.message,
+        payload: error.response?.data.message,
       });
     }
   };
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://ecommerce-backend-sable-tau.vercel.app",
+    },
+  },
   optimizeDeps: {
     exclude: ["country-state-picker"],
   },
